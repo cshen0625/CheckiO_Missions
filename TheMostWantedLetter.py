2ee9b2c0dde25cd048b59f6197e2f28895af4df6
Chen Shen <cshen@mori.m.is.nagoya-u.ac.jp>
2018-07-26 22:08:16 +0900

    Find out which is the most wanted letter

---
+++ TheMostWantedLetter.py
@@ -0,0 +1,27 @@
+def checkio(text):
+    count = 0
+    char_list = list(text.lower())
+    char_list.sort()
+    for x in char_list:
+        if x >= 'a' and x <= 'z':
+            if char_list.count(x) > count:
+                count = char_list.count(x)
+                ch = x
+
+    #replace this for solution
+    return ch
+
+if __name__ == '__main__':
+    print("Example:")
+    print(checkio("Hello World!"))
+
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio("Hello World!") == "l", "Hello test"
+    assert checkio("How do you do?") == "o", "O is most wanted"
+    assert checkio("One") == "e", "All letter only once."
+    assert checkio("Oops!") == "o", "Don't forget about lower case."
+    assert checkio("AAaooo!!!!") == "a", "Only letters."
+    assert checkio("abe") == "a", "The First."
+    print("Start the long test")
+    assert checkio("a" * 9000 + "b" * 1000) == "a", "Long."
+    print("The local tests are done.")
