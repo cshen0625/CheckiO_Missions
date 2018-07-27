a24fd412da8293995960929063d88a7507f4b1da
Chen Shen <cshen@mori.m.is.nagoya-u.ac.jp>
2018-07-27 19:08:22 +0900

    Trim an array down to its non-unique elements

---
+++ Non-uniqueElements.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+'''
+ Created on 2018-07-27 12:27:04
+@author: cshen
+'''
+def checkio(data):
+    tmp = []
+    for x in data:
+        if data.count(x) > 1:
+            tmp.append(x)
+    return tmp
+
+if __name__ == "__main__":
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert list(checkio([1, 2, 3, 1, 3])) == [1, 3, 1, 3], "1st example"
+    assert list(checkio([1, 2, 3, 4, 5])) == [], "2nd example"
+    assert list(checkio([5, 5, 5, 5, 5])) == [5, 5, 5, 5, 5], "3rd example"
+    assert list(checkio([10, 9, 10, 10, 9, 8])) == [10, 9, 10, 10, 9], "4th example"
+    print("It is all good. Let's check it now")
